d924e76aff24f2799f512112a42459925d917cb2
Rodrigopca42 <rodrigopca426281@gmail.com>
2023-07-07 11:24:44 -0300

    Criação dos primeiros componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CadastroComponent } from './components/pages/cadastro/cadastro.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    CadastroComponent,
+    AboutComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
