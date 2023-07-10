e514cf51c77a8936885183731a4bca7334edf427
Rodrigopca42 <rodrigopca426281@gmail.com>
2023-07-10 10:05:41 -0300

    Rotas criadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
+import { CadastroComponent } from './components/pages/cadastro/cadastro.component';
+import { AboutComponent } from './components/pages/about/about.component';
 
 const routes: Routes = [
-  {path:'', component: NavComponent}
+  {path:'', component: NavComponent, children: [
+    {path: 'home', component: HomeComponent},
+    {path: 'page/cadastro', component: CadastroComponent},
+    {path: 'page/about', component: AboutComponent}
+  ]}
 ];
 
 @NgModule({
