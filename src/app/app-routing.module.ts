8d2d178b384e7378162e9ca78762a5ee1bb0903a
Rodrigopca42 <rodrigopca426281@gmail.com>
2023-07-11 11:15:22 -0300

    Rota para login criado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,10 @@ import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
 import { CadastroComponent } from './components/pages/cadastro/cadastro.component';
 import { AboutComponent } from './components/pages/about/about.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
+  {path: 'login', component: LoginComponent},
   {path:'', component: NavComponent, children: [
     {path: 'home', component: HomeComponent},
     {path: 'page/cadastro', component: CadastroComponent},
