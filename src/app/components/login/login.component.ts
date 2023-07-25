6b0ac70d60325093afd94490369af2e07ad1d15c
“Rodrigopca42” <rodrigopca426281@gmail.com>
2023-07-25 15:41:55 -0300

    configuração de login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 
 import { Credenciais } from 'src/app/models/credenciais';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
  creds: Credenciais = {
   email:'',
@@ -19,12 +20,19 @@ export class LoginComponent {
  email = new FormControl(null, Validators.email);
  senha = new FormControl(null, Validators.minLength(3));
 
-  constructor(private toast: ToastrService){}
+  constructor(
+    private toast: ToastrService,
+    private service: AuthService
+    ){}
+
+  ngOnInit(): void {
+  }
 
 
  logar(){
-  this.toast.error('Usuário e/ou senha inválidos', 'Login');
-  this.creds.senha = '';
+  this.service.authenticate(this.creds).subscribe(resposta =>{
+    this.toast.info();
+  })
  }
 
  validaCampos(): boolean{
